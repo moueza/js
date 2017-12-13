affe14c6d9a9972ec5aa22e72006d4300e3dab78
moueza <mouezapeter@gmail.com>
2017-12-13 01:29:23 +0100

    sov

--- js.js
+++ js.js
@@ -1,3 +1,10 @@
+//https://stackoverflow.com/questions/11163060/cannot-set-property-innerhtml-of-null
+window.load= function(){
+    document.getElementById("resultat").innerHTML = 5;
+    console.log("in main js");
+    alert("in main JS alert");
 function dup(x){
-     document.getElementById('resultat').innerHTML = "New text!";
+  
+    console.log("in dup()");
+}
 }
