a6a74f4ddac567d3d9e6effa5d96ba1837b09bed
moueza <mouezapeter@gmail.com>
2017-12-22 20:54:32 +0100

    js file considered; button just transcient

--- js.js
+++ js.js
@@ -1,22 +1,43 @@
 //https://stackoverflow.com/questions/11163060/cannot-set-property-innerhtml-of-null
-window.load= function(){
+//load KO ou onload OK
+window.onload= function(){
     document.getElementById("resultat").innerHTML = 5;
     console.log("in main js");
-    alert("in main JS alert");
+    //alert("in main JS alert");
 
     
-    function dup(){
-	
-	console.log("in dup()");
+    function bouton(){
+	document.getElementById("boutonn").innerHTML =  "bouton pressé";
+	//pr 5sec
+	console.log("in bouton()");
     }
 
-      function(){
+    var funn = function(){
 	//closure
 	console.log("in dup()");
     }
-}
-  
+
+
     function dup2(){
 	
 	console.log("in dup()");
     }
+
+
+    function jsFile(){
+	document.getElementById("jsfileResult").innerHTML =  "js.js";
+	console.log("in jsFile");
+	
+
+    }
+    jsFile();
+
+    function callREST(){
+	
+	console.log("callREST");
+    }
+    function gameEngine(time){
+	
+	console.log("gameEngine");
+    }
+}
